55c663a896122dbe00e05354f153d4261fc30c8a
imnetcat <37350375+imnetcat@users.noreply.github.com>
2018-05-24 17:37:42 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,84 +1,61 @@
-var Hapi = require('hapi');
-var Joi = require('joi');
+var express = require("express");
+var bodyParser = require("body-parser");
+var mongodb = require("mongodb");
+var ObjectID = mongodb.ObjectID;
 
+var CONTACTS_COLLECTION = "contacts";
 
-var internals = {};
+var app = express();
+app.use(bodyParser.json());
 
+// Create a database variable outside of the database connection callback to reuse the connection pool in your app.
+var db;
 
-// Type shortcuts
+// Connect to the database before starting the application server.
+mongodb.MongoClient.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/test", function (err, client) {
+  if (err) {
+    console.log(err);
+    process.exit(1);
+  }
 
-var S = Joi.string;
-var N = Joi.number;
-var A = Joi.array;
+  // Save database object from the callback for reuse.
+  db = client.db();
+  console.log("Database connection ready");
 
+  // Initialize the app.
+  var server = app.listen(process.env.PORT || 8080, function () {
+    var port = server.address().port;
+    console.log("App now running on port", port);
+  });
+});
+// Generic error handler used by all endpoints.
+function handleError(res, reason, message, code) {
+  console.log("ERROR: " + reason);
+  res.status(code || 500).json({"error": message});
+}
 
+/*  "/api/contacts"
+ *    GET: finds all contacts
+ *    POST: creates a new contact
+ */
 
-internals.get = function (request, reply) {
-
-    reply('Success!\n');
-};
-
-
-internals.output = function (request, reply) {
-
-    reply({ myOutput: request.query.input });
-};
-
-
-internals.payload = function (request, reply) {
-
-    reply('Success!\n');
-};
-
-
-internals.echo = function (request, reply) {
-
-    reply(request.payload);
-};
-
-internals.redirect = function (request, reply) {
-
-    reply.redirect('/error');
-};
-
-internals.error = function (request, reply) {
-
-    reply('This is my error');
-};
-
-var server = new Hapi.Server(~~process.env.PORT || 3000, '0.0.0.0');
-
-server.route([
-    { method: 'GET', path: '/', config: { handler: internals.get, validate: { query: { username: S() } } } },
-    { method: 'POST', path: '/', config: { handler: internals.echo, payload: { parse: true } } },
-    { method: 'GET', path: '/admin', config: { handler: internals.get, validate: { query: { username: S().required(), password: S().required() } } } },
-    { method: 'GET', path: '/users', config: { handler: internals.get, validate: { query: { email: S().email().required().min(18) } } } },
-    { method: 'GET', path: '/config', config: { handler: internals.get, validate: { query: { choices: A().required() } } } },
-    { method: 'GET', path: '/test', config: { handler: internals.get, validate: { query: { num: N().min(0) } } } },
-    { method: 'GET', path: '/output', config: { handler: internals.output, validate: { query: { input: S().min(3) } } } },
-    { method: 'GET', path: '/users/{id}', config: { description: 'Get a user', handler: internals.get, validate: { params: { id: N().required() }, query: { name: S().description('the user name').required() } } } },
-    { method: 'GET', path: '/redirect', config: { handler: internals.redirect } },
-    { method: 'GET', path: '/error', config: { handler: internals.error } }
-]);
-
-var schema = Joi.object().keys({
-    title: S().required().invalid('director'),
-    status: S().required().valid('open', 'pending', 'close'),
-    participants: A().required().includes(S(), N()).min(2)
+app.get("/api/contacts", function(req, res) {
 });
 
-server.route({
-    method: 'POST',
-    path: '/users/{id}',
-    config: {
-        handler: internals.payload,
-        validate: {
-            payload: schema
-        }
-    }
+app.post("/api/contacts", function(req, res) {
 });
 
-server.start(function () {
+/*  "/api/contacts/:id"
+ *    GET: find contact by id
+ *    PUT: update contact by id
+ *    DELETE: deletes contact by id
+ */
 
-    console.log('Server started at [' + server.info.uri + ']');
+app.get("/api/contacts/:id", function(req, res) {
+});
+
+app.put("/api/contacts/:id", function(req, res) {
+});
+
+app.delete("/api/contacts/:id", function(req, res) {
 });
