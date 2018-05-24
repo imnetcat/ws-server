79311e7dd8c3b9fc5e72523d66b19c70a83e58ad
imnetcat <37350375+imnetcat@users.noreply.github.com>
2018-05-24 17:38:13 +0300

    Update server.js

--- server.js
+++ server.js
@@ -59,3 +59,28 @@ app.put("/api/contacts/:id", function(req, res) {
 
 app.delete("/api/contacts/:id", function(req, res) {
 });
+app.get("/api/contacts", function(req, res) {
+  db.collection(CONTACTS_COLLECTION).find({}).toArray(function(err, docs) {
+    if (err) {
+      handleError(res, err.message, "Failed to get contacts.");
+    } else {
+      res.status(200).json(docs);
+    }
+  });
+});
+
+app.post("/api/contacts", function(req, res) {
+  var newContact = req.body;
+
+  if (!req.body.name) {
+    handleError(res, "Invalid user input", "Must provide a name.", 400);
+  }
+
+  db.collection(CONTACTS_COLLECTION).insertOne(newContact, function(err, doc) {
+    if (err) {
+      handleError(res, err.message, "Failed to create new contact.");
+    } else {
+      res.status(201).json(doc.ops[0]);
+    }
+  });
+});
