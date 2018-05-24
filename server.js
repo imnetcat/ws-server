2e7493c78cf1f3d154e2dcc342620d870d21e8bf
imnetcat <37350375+imnetcat@users.noreply.github.com>
2018-05-24 19:51:22 +0300

    Update server.js

--- server.js
+++ server.js
@@ -2,27 +2,17 @@
 const path = require("path");
 const express = require("express");
 const http = require("http");
-const WebSocket = require("ws");
+const net = require("net");
 const app = express();
 
 const port = process.env.PORT || 3000;
-const host = process.env.HOST || '0.0.0.0';
+const host = 'https://ws-server-top.herokuapp.com'; //process.env.HOST || '0.0.0.0';
+concole.log('Server  open on ' + host + ':' + port);
 
-//initialize a simple http server
-const server = http.createServer(app);
-//initialize the WebSocket server instance
-const wss = new WebSocket.Server({ server });
-wss.on('connection', (ws) => {
-    //connection is up, let's add a simple simple event
-    ws.on('message', (message) => {
-        //log the received message and send it back to the client
-        console.log('received: %s', message);
-        ws.send('Echo -> ' + message);
-    });
-    //send immediatly a feedback to the incoming connection
-    ws.send('Hi there, I am a WebSocket server');
-});
-//start our server
-server.listen(port, host, () => {
-    console.log('Server opened on ' + server.address().address + ':' + server.address().port +' :3');
-});
+var socket = new net.Socket();
+socket.connect(port, host, () => {
+  concole.log('Server opened!');
+}
+               
+
+
