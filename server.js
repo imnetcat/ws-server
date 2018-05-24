7b1640788e0c30d5c537893c6683b5d990bb066b
imnetcat <37350375+imnetcat@users.noreply.github.com>
2018-05-24 13:55:17 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,22 +1,25 @@
-'use strict';
+"use strict";
 
-const net = require('net');
-const port = process.env.PORT || 3000;
+const express = require("express");
+const http = require("http");
+const WebSocket = require("ws");
+const app = express();
 
-const server = net.createServer((connection) => {
-  // 'connection' listener
-  console.log('Client connected');
-  connection.on('End', () => {
-    console.log('Client disconnected');
-  });
-  connection.write('Hello\r\n');
-  connection.pipe(connection);
+//initialize a simple http server
+const server = http.createServer(app);
+//initialize the WebSocket server instance
+const wss = new WebSocket.Server({ server });
+wss.on('connection', (ws) => {
+    //connection is up, let's add a simple simple event
+    ws.on('message', (message) => {
+        //log the received message and send it back to the client
+        console.log('received: %s', message);
+        ws.send(`Hello, you sent -> ${message}`);
+    });
+    //send immediatly a feedback to the incoming connection
+    ws.send('Hi there, I am a WebSocket server');
 });
-
-server.on('Error', (err) => {
-  throw err;
-});
-
-server.listen(port, () => {
-  console.log('Server opened on ', server.address());
+//start our server
+server.listen(process.env.PORT || 3000, () => {
+    console.log(`Server started on port ${server.address().port} :)`);
 });
