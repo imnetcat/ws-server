5ec8c8e426ec604029609798d7d01f894592febb
imnetcat <37350375+imnetcat@users.noreply.github.com>
2018-05-28 20:17:20 +0300

    Update server.js

--- server.js
+++ server.js
@@ -3,8 +3,10 @@
 const express = require("express");
 const http = require("http");
 const WebSocket = require("ws");
+const path = require("path");
 const app = express();
 const PORT = process.env.PORT;
+const INDEX = path.join(__dirname, "index.html");
 
 //initialize a simple http server
 const server = express()
