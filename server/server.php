f2f5a83b07821eed51ddbf206f17e6f9c291b99b
imnetcat <37350375+imnetcat@users.noreply.github.com>
2018-06-02 19:08:25 +0300

    Update server.php

--- server/server.php
+++ server/server.php
@@ -6,9 +6,8 @@
 </head>
 <body>
 <br><br>
-
-<script src="socket.js" type="text/javascript"></script>
-
+<br><br>
+<br><br>
 Server address: <br><br>
 Server port: 
 <br /><br />
@@ -23,8 +22,6 @@ error_reporting(E_ALL); //Выводим все ошибки и предупре
 set_time_limit(0);		//Время выполнения скрипта не ограничено
 ob_implicit_flush();	//Включаем вывод без буферизации
 
-$starttime = round(microtime(true),2);
-echo "GO() ... <br />\r\n";
 
 echo "socket_create ...";
 if(!$socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP)){
@@ -35,9 +32,8 @@ if(!$socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP)){
 }
 
 
-echo "socket_bind on" . "$_SERVER['SERVER_ADDR']" . ":" . "$_SERVER['SERVER_PORT']" . "...";
-$bind = socket_bind($socket, '127.0.0.1', 15777);//привязываем его к указанным ip и порту
-if($bind < 0){
+echo "socket_bind on" . $_SERVER['SERVER_ADDR'] . ":" . $_SERVER['SERVER_PORT'] . "...";
+if(!socket_bind($socket, $_SERVER['SERVER_ADDR'], $_SERVER['SERVER_PORT'])){
   echo "Error: ".socket_strerror(socket_last_error())."<br />\r\n";
   exit();
 }else{
@@ -47,9 +43,7 @@ if($bind < 0){
 socket_set_option($socket, SOL_SOCKET, SO_REUSEADDR, 1);//разрешаем использовать один порт для нескольких соединений
 
 echo "Listening socket... ";
-$listen = socket_listen($socket, 5);//слушаем сокет
-
-if($listen < 0){
+if(!socket_listen($socket, 5)){
   echo "Error: ".socket_strerror(socket_last_error())."<br />\r\n";
   exit();
 }else{
@@ -72,11 +66,6 @@ while(true){ //Бесконечный цикл ожидания подключе
   socket_write($accept, $msg);
   echo "OK <br />\r\n";
 	
-  if( ( round(microtime(true),2) - $starttime) > 100) {
-    echo "time = ".(round(microtime(true),2) - $starttime);
-    echo "return <br />\r\n";
-    socket_close($socket);
-  }
 }
 
 ?>
