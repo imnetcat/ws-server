2a2558bfd73ecba0c2beba23092681605b9aa5fd
imnetcat <37350375+imnetcat@users.noreply.github.com>
2018-06-02 15:21:03 +0300

    Update socket.js

--- socket.js
+++ socket.js
@@ -1,36 +1,34 @@
 "use strict"; //All my JavaScript written in Strict Mode http://ecma262-5.com/ELS5_HTML.htm#Annex_C
 
-(function () {
-    // ======== private vars ========
-	var socket;
+  var socket;
 
-    ////////////////////////////////////////////////////////////////////////////
-    var init = function () {
-		
+  window.addEventListener('load', function () {
+    init();
+  }, false);
+
+  var init = function () {
 		socket = new WebSocket(document.getElementById("sock-addr").value);
-                socket.onclose = document.getElementById("sock-info").innerHTML += "Соединение закрыто ^^<br />";
-		socket.onopen = connectionOpen; 
-		socket.onmessage = messageReceived; 
-		//socket.onerror = errorOccurred; 
+    socket.onclose = document.getElementById("sock-info").innerHTML += "Соединение закрыто ^^<br />";
+		socket.onopen = connectionOpen;
+		socket.onmessage = messageReceived;
+		//socket.onerror = errorOccurred;
 		//socket.onopen = connectionClosed;
-
-        document.getElementById("sock-send-butt").onclick = function () {
-            socket.send(document.getElementById("sock-msg").value);
-        };
-
-
-        document.getElementById("sock-disc-butt").onclick = function () {
-            connectionClose();
-        };
-
-        document.getElementById("sock-recon-butt").onclick = function () {
-            socket = new WebSocket(document.getElementById("sock-addr").value);
-            socket.onopen = connectionOpen;
-            socket.onmessage = messageReceived;
-        };
-
+		
+    document.getElementById("sock-send-butt").onclick = function () {
+      socket.send(document.getElementById("sock-msg").value);
     };
 
+    document.getElementById("sock-disc-butt").onclick = function () {
+      socket.close();
+      document.getElementById("sock-info").innerHTML += "Соединение закрыто <br />";
+    };
+
+    document.getElementById("sock-recon-butt").onclick = function () {
+      socket = new WebSocket(document.getElementById("sock-addr").value);
+      socket.onopen = connectionOpen;
+      socket.onmessage = messageReceived;
+    };
+  };
 
 	function connectionOpen() {
 	   socket.send("Connection with \""+document.getElementById("sock-addr").value+"\" Подключение установлено обоюдно, отлично!");
@@ -40,21 +38,3 @@
 	    console.log("Ответ сервера: " + e.data);
         document.getElementById("sock-info").innerHTML += (e.data+"<br />");
 	}
-
-    function connectionClose() {
-        socket.close();
-        document.getElementById("sock-info").innerHTML += "Соединение закрыто <br />";
-
-    }
-
-
-    return {
-        ////////////////////////////////////////////////////////////////////////////
-        // ---- onload event ----
-        load : function () {
-            window.addEventListener('load', function () {
-                init();
-            }, false);
-        }
-    }
-})().load();
