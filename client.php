19cbecd2a1ca0493461cb9b943b1eb140c54859b
imnetcat <37350375+imnetcat@users.noreply.github.com>
2018-06-02 18:13:59 +0300

    Update client.php

--- client.php
+++ client.php
@@ -5,7 +5,7 @@
   <title>Siple Web-Socket Client</title>
   <script async>
     document.addEventListener('DOMContentLoaded', () => {
-      document.detElementById('to_server').onclick( () => {
+      document.getElementById('to_server').onclick( () => {
         document.location = '/server/server.php';
       });
     });
