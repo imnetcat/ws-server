cd1a310d72c35d27a0b7dae458e018f39f183b65
imnetcat <37350375+imnetcat@users.noreply.github.com>
2018-06-04 00:53:20 +0300

    Update server_actions.php

--- server/server_actions.php
+++ server/server_actions.php
@@ -10,7 +10,7 @@ switch ($_POST['action']){
       case 'create':
       $addr = $_POST['addr'];
       $port = $_POST['port'];
-      $create_log = create($addr, $port);
+      $create_log = "Error";
       if($create_log == "Error"){
         echo "Error: " . socket_strerror(socket_last_error()) . "<br>";
       }else{
