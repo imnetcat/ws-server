8abf9f3eac28824191c389065ee29b5c86f9f7ab
imnetcat <37350375+imnetcat@users.noreply.github.com>
2018-06-04 01:34:26 +0300

    Update server_functions.php

--- server/server_functions.php
+++ server/server_functions.php
@@ -1,10 +1,10 @@
 <?
 
 function create($addr, $port){
-  if(!$socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP)){
-    return "Error";
-  } else {
+  if($socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP)){
     return "Success";
+  }else{
+    return "Error" . socket_strerror(socket_last_error());
   }
 }
 
