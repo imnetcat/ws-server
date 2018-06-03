d352d8e8d147a2f9a0c4381155bdf79a04f25e87
imnetcat <37350375+imnetcat@users.noreply.github.com>
2018-06-04 00:31:43 +0300

    Create server_actions.php

---
+++ server/server_actions.php
@@ -0,0 +1,11 @@
+inlude_once "server_functions.php";
+
+switch ($_POST['action']){
+      case 'create':
+      $addr = $_POST['addr'];
+      $port = $_POST['port'];
+      echo create($addr, $port);
+      break;
+    case '':
+      break;
+};
