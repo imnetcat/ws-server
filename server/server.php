ec49e01cf2a3ecad5f1659160e74d17278e088ab
imnetcat <37350375+imnetcat@users.noreply.github.com>
2018-06-03 23:27:35 +0300

    Update server.php

--- server/server.php
+++ server/server.php
@@ -16,6 +16,17 @@ Server port:
 
 Полученные сообщения от сервера: 
 <div id="sock-info" style="border: 1px solid">
- </div>
+<?
+error_reporting(E_ALL); //Выводим все ошибки и предупреждения
+set_time_limit(0);		//Время выполнения скрипта не ограничено
+ob_implicit_flush();	//Включаем вывод без буферизации
+echo "socket_create ...";
+if(!$socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP)){
+  echo "Error: ".socket_strerror(socket_last_error())."<br />\r\n";
+  exit();
+} else {
+  echo "OK <br />\r\n";
+}
+</div>
 </body>
 </html>
