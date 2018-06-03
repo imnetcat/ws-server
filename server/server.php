bf57ee9b73e8812b07465fab0328f5b5d841d21d
imnetcat <37350375+imnetcat@users.noreply.github.com>
2018-06-04 00:26:18 +0300

    Update server.php

--- server/server.php
+++ server/server.php
@@ -3,8 +3,20 @@
 <head>
   <meta charset="UTF-8" />
   <title>Siple Web-Socket Client</title>
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-  <script async>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+</head>
+<body>
+<br><br>
+<br><br>
+<br><br>
+Server address: <span id="addr"> <? echo $address = $_SERVER['SERVER_ADDR']; ?> </span>
+<br><br>
+Server port: <span id="port"><? echo $port = getservbyname('socks', 'tcp');
+  ?></span>
+<br><br>
+<br><br>
+<button id="startbtn">Start server</button>
+<script async>
     $( () => {
       $('#startbtn').click( () => {
         $.ajax({
@@ -22,19 +34,6 @@
       });
     });
   </script>
-</head>
-<body>
-<br><br>
-<br><br>
-<br><br>
-Server address: <span id="addr"> <? echo $address = $_SERVER['SERVER_ADDR']; ?> </span>
-<br><br>
-Server port: <span id="port"><? echo $port = getservbyname('socks', 'tcp');
-  ?></span>
-<br><br>
-<br><br>
-<button id="startbtn">Start server</button>
-  
 Полученные сообщения от сервера: 
 <div id="logs" style="border: 1px solid">
 
