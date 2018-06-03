5a5cb29a922487014dc64317fde6458fb6f26e69
imnetcat <37350375+imnetcat@users.noreply.github.com>
2018-06-04 01:20:14 +0300

    Update server.php

--- server/server.php
+++ server/server.php
@@ -28,7 +28,7 @@ Server port: <span id="port"><? echo $port = getservbyname('socks', 'tcp');
 		      	port: '<? echo $port ?>' ,
 		      },
 		      success: function(data){
-            $('#logs').html(data);
+            $('#logs').append($("<p>" + data + "</p>"));
           }
 	});
       });
