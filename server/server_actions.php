a0ead8df60273812b1f406b1333eca183f58322f
imnetcat <37350375+imnetcat@users.noreply.github.com>
2018-06-04 20:48:13 +0300

    Update server_actions.php

--- server/server_actions.php
+++ server/server_actions.php
@@ -3,16 +3,15 @@
 require_once "server_functions.php";
 
 switch ($_POST['action']){
-      case 'create':
-      $addr = $_POST['addr'];
-      $port = $_POST['port'];
-      echo create($addr, $port);
-      break;
-      case 'bind':
-      break;
-      case 'listen':
-      break;
-      
+  case 'bind':
+    echo bind($_POST['socket'], $_POST['address'], $_POST['port']);
+  break;
+  case 'listen':
+    echo listen($_POST['socket']);
+  break;
+  case 'connect':
+    echo connect($_POST['socket']);
+  break; 
 };
 
 ?>
