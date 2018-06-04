8e10d88768fc848834c801d788265f52fc76072e
imnetcat <37350375+imnetcat@users.noreply.github.com>
2018-06-04 23:58:29 +0300

    Update server_actions.php

--- server/server_actions.php
+++ server/server_actions.php
@@ -12,18 +12,17 @@ switch ($_POST['action']){
     }
   break;
   case 'bind':  
-    function bind($_POST['address'], $_POST['port']){
   if(!$socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP)){
     return "Error";
   } else {
-    if(!socket_bind($socket, $address, $port)){
+    if(!socket_bind($socket, $_POST['address'], $_POST['port'])){
       return "Error: " . socket_strerror(socket_last_error());
     }else{
       return "OK";
     }
   }
   socket_close($socket);
-}   
+ 
   break;
   case 'listen':
     if(!$socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP)){
