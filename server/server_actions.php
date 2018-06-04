d1d23e4efa01f2cd1b7bec81cfc80eab25e94ed4
imnetcat <37350375+imnetcat@users.noreply.github.com>
2018-06-04 23:28:12 +0300

    Update server_actions.php

--- server/server_actions.php
+++ server/server_actions.php
@@ -5,20 +5,34 @@ require_once "server_functions.php";
 switch ($_POST['action']){
   case 'create':
     if(!$socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP)){
-      echo "Error: " . socket_strerror(socket_last_error());
+      echo "Error";
     } else {
       echo "Success";
-      $_SERVER['socket'] = $socket;
     }
   break;
   case 'bind':
-    echo bind($_SERVER['socket'], $_POST['address'], $_POST['port']);
+    if(!$socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP)){
+      echo "Error";
+    } else {
+      echo bind($socket, $_POST['address'], $_POST['port']);
+    }
   break;
   case 'listen':
-    echo listen($_SERVER['socket']);
+    if(!$socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP)){
+      echo "Error";
+    } else {
+      bind($socket, $_POST['address'], $_POST['port']);
+      echo listen($socket);
+    }
   break;
   case 'connect':
-    echo connect($_SERVER['socket']);
+    if(!$socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP)){
+      echo "Error";
+    } else {
+      bind($socket, $_POST['address'], $_POST['port']);
+      listen($socket);
+      echo connect($socket);
+    }
   break; 
 };
 
