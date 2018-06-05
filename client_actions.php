cd74690ba210f7469b8be8afa33a34e01881f8a1
imnetcat <37350375+imnetcat@users.noreply.github.com>
2018-06-05 17:23:27 +0300

    Create client_actions.php

---
+++ client_actions.php
@@ -0,0 +1,30 @@
+<?
+require_once "server_functions.php";
+switch ($_POST['action']){
+  case 'create':
+    if(!$socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP)){
+      echo "Error";
+    } else {
+      socket_close($socket);
+      echo "OK";
+    }
+  break;
+  case 'bind':
+    if(!$socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP)){
+      echo "Error";
+    } else {
+      echo bind($socket, $_POST['address'], $_POST['port']);
+      socket_close($socket);
+    }
+  break;
+  case 'connect':
+    if(!$socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP)){
+      echo "Error";
+    } else {
+      bind($socket, $_POST['address'], $_POST['port']);
+      echo connect($socket);
+      socket_close($socket);
+    }
+  break;
+};
+?>
