dc6739a7f6a5d6a6e5ff154f684d953e8e8bf2ce
imnetcat <37350375+imnetcat@users.noreply.github.com>
2018-06-06 18:36:49 +0300

    Update client_actions.php

--- client_actions.php
+++ client_actions.php
@@ -9,19 +9,10 @@ switch ($_POST['action']){
       echo "OK";
     }
   break;
-  case 'bind':
-    if(!$socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP)){
-      echo "Error";
-    } else {
-      echo bind($socket, $_POST['address'], $_POST['port']);
-      socket_close($socket);
-    }
-  break;
   case 'connect':
     if(!$socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP)){
       echo "Error";
     } else {
-      bind($socket, $_POST['address'], $_POST['port']);
       echo connect($socket, $_POST['address'], $_POST['port']);
       socket_close($socket);
     }
