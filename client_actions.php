2a4f7320d6c0e2e5c1743058b712d4debf159a40
imnetcat <37350375+imnetcat@users.noreply.github.com>
2018-06-06 18:44:33 +0300

    Update client_actions.php

--- client_actions.php
+++ client_actions.php
@@ -1,21 +1,50 @@
 <?
 require_once "client_functions.php";
 switch ($_POST['action']){
-  case 'create':
-    if(!$socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP)){
-      echo "Error";
-    } else {
-      socket_close($socket);
-      echo "OK";
-    }
-  break;
   case 'connect':
-    if(!$socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP)){
-      echo "Error";
-    } else {
-      echo connect($socket, $_POST['address'], $_POST['port']);
-      socket_close($socket);
-    }
+error_reporting(E_ALL);
+
+echo "<h2>Соединение TCP/IP</h2>\n";
+
+/* Получаем порт сервиса WWW. */
+$service_port = getservbyname('www', 'tcp');
+
+/* Получаем IP-адрес целевого хоста. */
+$address = gethostbyname('www.example.com');
+
+/* Создаём сокет TCP/IP. */
+$socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
+if ($socket === false) {
+    echo "Не удалось выполнить socket_create(): причина: " . socket_strerror(socket_last_error()) . "\n";
+} else {
+    echo "OK.\n";
+}
+
+echo "Пытаемся соединиться с '$address' на порту '$service_port'...";
+$result = socket_connect($socket, $address, $service_port);
+if ($result === false) {
+    echo "Не удалось выполнить socket_connect().\nПричина: ($result) " . socket_strerror(socket_last_error($socket)) . "\n";
+} else {
+    echo "OK.\n";
+}
+
+$in = "HEAD / HTTP/1.1\r\n";
+$in .= "Host: www.example.com\r\n";
+$in .= "Connection: Close\r\n\r\n";
+$out = '';
+
+echo "Отправляем HTTP-запрос HEAD...";
+socket_write($socket, $in, strlen($in));
+echo "OK.\n";
+
+echo "Читаем ответ:\n\n";
+while ($out = socket_read($socket, 2048)) {
+    echo $out;
+}
+
+echo "Закрываем сокет...";
+socket_close($socket);
+echo "OK.\n\n";
   break;
 };
 ?>
