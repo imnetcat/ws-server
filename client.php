3714cbd243f5563a4035bdd9d423a325ceb33c0d
imnetcat <37350375+imnetcat@users.noreply.github.com>
2018-06-06 18:48:02 +0300

    Update client.php

--- client.php
+++ client.php
@@ -30,35 +30,8 @@ $( () => {
   $('#startbtn').click( () => {
     $('#logs').append($("<span>Creating ...</span><br>"));
     $.ajax({
-      type: "POST",
-      url: "client_actions.php",
-      data: {
-        action: 'create'
-      },
-      success: function(data){
-        if(data != "OK"){
-          $('#logs').append($("<span>" + data + "</span><br>"));
-        }else{
-          $('#logs').append($("<span>" + data + "</span><br>"));
-          $('#logs').append($("<span>Connecting...</span><br>"));
-          $.ajax({
-            type: "POST",
-            url: "client_actions.php",
-            data: {
-              action: 'connect',
-              address: '<? echo $server_address ?>',
-              port: <? echo $port ?>
-            },
-            success: function(data){
-              if(data != "OK"){
-                $('#logs').append($("<span>" + data + "</span><br>"));
-              }else{
-                $('#logs').append($("<span>Connected!</span><br>"));
-              }
-            }
-          });
-        }
-      }
+      type: "GET",
+      url: "client_actions.php"    
     });
   });
 });
