04a84f78f8553f3d8d2a43cf81cbebe9035957c3
imnetcat <37350375+imnetcat@users.noreply.github.com>
2018-06-06 14:50:53 +0300

    Update server.php

--- server/server.php
+++ server/server.php
@@ -2,7 +2,7 @@
 <html>
 <head>
   <meta charset="UTF-8" />
-  <title>Siple Web-Socket Client</title>
+  <title>Server</title>
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 </head>
 <body>
