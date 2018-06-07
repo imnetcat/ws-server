12ef2caaf6a263dbfd6a1733d8dd8cd30d899e74
imnetcat <37350375+imnetcat@users.noreply.github.com>
2018-06-07 19:02:14 +0300

    Update server.php

--- server/server.php
+++ server/server.php
@@ -10,12 +10,73 @@
 <br><br>
 <? if(extension_loaded('sockets')){ echo "Status ext-sockets: ready to use"; }else{ echo "Status ext-sockets: UNVALIBLE !"; } ?> 
 <br><br>
-Server address: <span id="addr"> <? echo $address =  gethostbyname('logs.net-cat-server.online'); ?> </span>
+Server address: <span id="addr"> <? echo $address =  $_SERVER['SERVER_ADDR']; ?> </span>
 <br><br>
 Server port: <span id="port"><? echo $port = getservbyname('socks', 'tcp'); ?></span>
 <br><br>
 <br><br>
-
+<button id="startbtn">Start server</button>
+<script async>
+    $( () => {
+      $('#startbtn').click( () => {
+	$('#logs').append($("<p>socket_create ...</p>"));
+	$.ajax({
+          type: "POST",
+	  url: "server_actions.php",
+	  data: {
+	    action: 'create'
+	  },
+	  success: function(data){
+            $('#logs').append($("<p>" + data + "</p>"));
+            $('#logs').append($("<p>socket_bind...</p>"));
+	    if(data != "OK"){
+	    }else{
+              $.ajax({
+              type: "POST",
+	      url: "server_actions.php",
+	      data: {
+	        action: 'bind',
+		address: '<? echo $address ?>',
+		port: <? echo $port ?>
+              },
+	      success: function(data){
+                $('#logs').append($("<p>" + data + "</p>"));
+	        $('#logs').append($("<p>Listening socket...</p>"));
+	        if(data != "OK"){
+	        }else{
+	          $.ajax({
+                  type: "POST",
+	          url: "server_actions.php",
+	          data: {
+	            action: 'listen',
+                  },
+	          success: function(data){
+                    $('#logs').append($("<p>" + data + "</p>"));
+	            $('#logs').append($("<p>Waiting...</p>"));
+	            if(data != "OK"){
+		    }else{
+	              $.ajax({
+                      type: "POST",
+	              url: "server_actions.php",
+	              data: {
+	                action: 'connect',
+                      },
+	              success: function(data){
+                        $('#logs').append($("<p>" + data + "</p>"));
+	              }
+		      });
+		    }
+		  }
+		  });
+		}
+	      }
+	      });
+	    }
+          }
+	});
+      });
+    });
+  </script>
 Полученные сообщения от сервера: 
 <div id="logs" style="border: 1px solid">
 
