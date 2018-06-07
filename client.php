b696236763592d6c0f77d60f3d410ad7759fce43
imnetcat <37350375+imnetcat@users.noreply.github.com>
2018-06-07 17:06:46 +0300

    Update client.php

--- client.php
+++ client.php
@@ -35,41 +35,25 @@ Message:
 
 Полученные сообщения от веб-сокета: 
 <div id="logs" style="border: 1px solid"></div>
-  <?
-error_reporting(E_ALL);
-echo "<h2>Соединение TCP/IP</h2>\n";
-/* Получаем порт сервиса WWW. */
-$service_port = getservbyname('socks', 'tcp');
-/* Получаем IP-адрес целевого хоста. */
-$address = gethostbyname('logs.net-cat-server.online');
-/* Создаём сокет TCP/IP. */
-$socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
-if ($socket === false) {
-    echo "Не удалось выполнить socket_create(): причина: " . socket_strerror(socket_last_error()) . "\n";
+  <?php
+
+$fp = fsockopen("master-web.com.ua", 80, $errno, $errstr, 30);
+if (!$fp) {
+      echo "$errstr ($errno)
+";
 } else {
-    echo "OK.\n";
-}
-echo "Пытаемся соединиться с '$address' на порту '$service_port'...";
-$result = socket_connect($socket, $address, $service_port);
-if ($result === false) {
-    echo "Не удалось выполнить socket_connect().\nПричина: ($result) " . socket_strerror(socket_last_error($socket)) . "\n";
-} else {
-    echo "OK.\n";
-}
-$in = "HEAD / HTTP/1.1\r\n";
-$in .= "Host: www.example.com\r\n";
-$in .= "Connection: Close\r\n\r\n";
-$out = '';
-echo "Отправляем HTTP-запрос HEAD...";
-socket_write($socket, $in, strlen($in));
-echo "OK.\n";
-echo "Читаем ответ:\n\n";
-while ($out = socket_read($socket, 2048)) {
-    echo $out;
-}
-echo "Закрываем сокет...";
-socket_close($socket);
-echo "OK.\n\n";
-  ?>
+      $query = "GET / HTTP/1.1 ";
+      $query .= "Upgrade: WebSocket";
+      $query .= "Connection: Upgrade";
+      $query .= "Host: logs.net-cat-server.online ";
+      fwrite($fp, $query);
+      $page = '';
+      while (!feof($fp)) {
+         $page .= fgets($fp, 4096);
+      }
+   fclose($fp);
+   if (!empty($page)) echo '<pre>'.$page.'</pre>';
+
+?>
 </body>
 </html>
