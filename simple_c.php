3b8dba89ca9487fc50f89c6eb75abb7ea974f24d
imnetcat <37350375+imnetcat@users.noreply.github.com>
2018-06-08 00:17:30 +0300

    Update simple_c.php

--- simple_c.php
+++ simple_c.php
@@ -3,7 +3,7 @@
 <body>
     <div id="root"></div>
     <script>
-        var host = 'ws://<? echo $_SERVER['SERVER_ADDR']?>:<? echo getservbyname('socks', 'tcp')?>/simple.php';
+        var host = 'ws://<? echo $_SERVER['SERVER_ADDR']?>:1080/simple.php';
         var socket = new WebSocket(host);
         socket.onmessage = function(e) {
             document.getElementById('root').innerHTML = e.data;
