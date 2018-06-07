a51659357e361bd43466b692cf09d44605fb3853
imnetcat <37350375+imnetcat@users.noreply.github.com>
2018-06-07 19:10:59 +0300

    Update server.php

--- server/server.php
+++ server/server.php
@@ -1,23 +1,3 @@
-<!DOCTYPE html>
-<html>
-<head>
-  <meta charset="UTF-8" />
-  <title>Server</title>
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-</head>
-<body>
-<br><br>
-<br><br>
-<? if(extension_loaded('sockets')){ echo "Status ext-sockets: ready to use"; }else{ echo "Status ext-sockets: UNVALIBLE !"; } ?> 
-<br><br>
-Server address: <span id="addr"> <? echo $address =  $_SERVER['SERVER_ADDR']; ?> </span>
-<br><br>
-Server port: <span id="port"><? echo $port = getservbyname('socks', 'tcp'); ?></span>
-<br><br>
-<br><br>
-<button id="startbtn">Start server</button>
-Полученные сообщения от сервера: 
-<div id="logs" style="border: 1px solid">
 <?php
 function go(){
 	$starttime = round(microtime(true),2);
@@ -98,6 +78,3 @@ if (isset($socket)){
     echo "OK <br />\r\n";
 }
 ?>
-</div>
-</body>
-</html>
