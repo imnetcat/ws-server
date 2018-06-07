9b4a5cacd339124360ffb5b13973cc9ac81c61f8
imnetcat <37350375+imnetcat@users.noreply.github.com>
2018-06-07 17:53:46 +0300

    Create ind_serv.php

---
+++ ind_serv.php
@@ -0,0 +1,15 @@
+<?php
+
+    $socket = stream_socket_server("tcp://0.0.0.0:8000", $errorNumber, $errorDescription);
+
+    if (!$socket) {
+        die("$errorDescription ($errorNumber)\n");
+    }
+
+    while ($connect = stream_socket_accept($socket, -1)) {
+        fwrite($connect, "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\nConnection: close\r\n\r\nHi! its work!\n\n");
+        fclose($connect);
+    }
+
+    fclose($socket);
+    ?>
