0385c656e2a5009faa83d208afe63e606a8662ea
imnetcat <37350375+imnetcat@users.noreply.github.com>
2018-06-08 11:35:25 +0300

    Update simple_c.php

--- simple_c.php
+++ simple_c.php
@@ -1,11 +1,8 @@
 <?
 $request = "GET /simple.php HTTP/1.1";
 $request .= "Host: logs.net-cat-server.online";
-$request .= "Upgrade: websocket";
-$request .= "Connection: Upgrade";
-$request .= "User-Agent: Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.170 Safari/537.36 OPR/53.0.2907.88 (Edition Campaign 33)";
-$request .= "Sec-WebSocket-Key: dGhlIHNhbXBsZSBub25jZQ==";
-$request .= "Sec-WebSocket-Protocol: chat, superchat";
-$request .= "Sec-WebSocket-Version: 13";
+$request .= "Connection: close";
+$request .= "User-Agent: Mozilla/5.0 (X11; U; Linux i686; ru; rv:1.9b5) Gecko/2008050509 Firefox/3.0b5";
+$request .= "Accept: text/html";
 header($request);
 ?>
