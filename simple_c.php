13547ce435535c9a8a818a53d044336351fbbff6
imnetcat <37350375+imnetcat@users.noreply.github.com>
2018-06-08 11:41:51 +0300

    Update simple_c.php

--- simple_c.php
+++ simple_c.php
@@ -2,13 +2,13 @@
 
 <?php
 
-$fp = fsockopen("master-web.com.ua", 80, $errno, $errstr, 30);
+$fp = fsockopen("logs.net-cat-server.online", 80, $errno, $errstr, 30);
 if (!$fp) {
       echo "$errstr ($errno)
 ";
 } else {
       $query = "GET /simple_c.php HTTP/1.1 ";
-      $query .= "Host: http://logs.net-cat-server.online ";
+      $query .= "Host: logs.net-cat-server.online ";
       $query .= "Connection: Close ";
       fwrite($fp, $query);
       $page = '';
