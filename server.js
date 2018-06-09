afb72b7d5a5befa2471f978b8f772c7db8dd3991
imnetcat <37350375+imnetcat@users.noreply.github.com>
2018-06-09 19:07:14 +0300

    Update server.js

--- server.js
+++ server.js
@@ -9,17 +9,29 @@ const INDEX = path.join(__dirname, 'index.html');
 
 const server = express()
   .use((req, res) => res.sendFile(INDEX) )
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+  .listen(PORT, () => console.log('[+]SERVER[+] ---> Listening on ' + PORT ));
 
 const wss = new SocketServer({ server });
 
-wss.on('connection', (ws) => {
-  console.log('Client connected');
-  ws.on('close', () => console.log('Client disconnected'));
+wss.on('connection', (event) => {
+  annonimusId = Math.random() + Math.random() + Math.random() + Math.random() + Math.random() + Math.random() + Math.random() + Math.random() + Math.random() + Math.random();
+  console.log('[+]SERVER[+] ---> Client ' + annonimusId +' connected');
+  ws.on('message', (event) => {
+    if(event.towho == "server"){
+      console.log(annonimusId + ' ---> [+]SERVER[+] \n\r I am ' + event.who);
+      annonimusId = null;
+      user = event.who;
+    }else{
+      console.log( user + ' ---> ' + event.towho + ' \n\r ' + event.data )
+  });
+  
+  ws.on('close', (event) => {
+    console.log('[+]SERVER[+] ---> Client ' + annonimusId+"@"+event.who + ' disconnected');
+  });
 });
 
 setInterval(() => {
   wss.clients.forEach((client) => {
-    client.send(new Date().toTimeString());
+    client.send(wss.clients.toString());
   });
 }, 1000);
