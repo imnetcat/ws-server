d424b1e6c090365fd7872501f21e9421be0645b2
imnetcat <37350375+imnetcat@users.noreply.github.com>
2018-06-12 00:08:51 +0300

    Update server.js

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ wss.on('connection', (sock) => {
   //var annonimusId = '[C]-' + (wss.clients.lenght + 1);
   console.log('[S] ---> Client ' + annonimusId +' connected');
   sock.on('message', (sock) => {
-    var sock = JSON.parse(sock.data"){
+    var sock = JSON.parse(sock.data){
     }else{
       console.log( user + ' ---> ' + sock.data.towho + ' |:| ' + sock.data.data );
     }
