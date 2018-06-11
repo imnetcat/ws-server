93aa4c2e28bc7c94f65eb94f9aa58e11b63c5efc
imnetcat <37350375+imnetcat@users.noreply.github.com>
2018-06-11 22:31:44 +0300

    Create server.php

---
+++ server.php
@@ -0,0 +1,16 @@
+<?
+
+$socket = stream_socket_server("tcp://0.0.0.0:8000", $errno, $errstr);
+
+if (!$socket) {
+    die("$errstr ($errno)\n");
+}
+
+while ($connect = stream_socket_accept($socket, -1)) {
+    fwrite($connect, "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\nConnection: close\r\n\r\nПривет");
+    fclose($connect);
+}
+
+fclose($socket);
+
+?>
